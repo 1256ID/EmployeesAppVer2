5327f19bd0c663b82fcd90a6c516f49884743529
Fredrik Norgren <fredrik_norgren@hotmail.com>
2025-05-20 12:01:25 +0200

    Added ViewModels

---
+++ EmployeesApp.Web/Views/Employees/CreateVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApp.Web.Views.Employees
+{
+    public class CreateVM
+    {
+        public required string Name { get; set; }
+        [Required(ErrorMessage = "Enter an email")]
+        [Display(Name = "Name", Prompt = "Name")]
+        public required string  Email { get; set; }
+        [Range(4,4, ErrorMessage = "Wrong number")]
+        [Display(Name = "Name", Prompt = "What is 2 + 2?")]
+        public int BotCheck { get; set; }
+    }
+}
---
+++ EmployeesApp.Web/Views/Employees/IndexVM.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeesApp.Web.Views.Employees
+{
+    public class IndexVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Email { get; set; }
+        public bool ShowAsHighlighted { get; set; }
+    }
+}
